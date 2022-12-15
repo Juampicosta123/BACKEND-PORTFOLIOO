b4a4d54bd0642b1b0db7819250ab8b50d993b0be
Juampicosta123 <juampicosta81@gmail.com>
2022-12-15 01:35:52 -0300

    first commit

---
+++ src/main/java/com/portfolio/portfolio/dto/dtohys.java
@@ -0,0 +1,47 @@
+package com.portfolio.portfolio.dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class dtohys {
+   @NotBlank
+   private String nombre;
+   @NotBlank
+   private int porcentaje; 
+   @NotBlank
+   private String img; 
+
+    public dtohys() {
+    }
+
+    public dtohys(String nombre, int porcentaje, String img) {
+        this.nombre = nombre;
+        this.porcentaje = porcentaje;
+        this.img = img;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(int porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+   
+   
+}
