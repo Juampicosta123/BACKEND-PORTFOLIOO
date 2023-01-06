8faa7afeb7ea8a3293c86c188f2921853c28cc01
Juampicosta123 <juampicosta81@gmail.com>
2023-01-06 17:05:41 -0300

    hola

--- src/main/java/com/portfolio/portfolio/Controller/CProyectos.java
+++ src/main/java/com/portfolio/portfolio/Controller/CProyectos.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/proyectos")
 //@CrossOrigin(origins = "http://localhost:4200")
 @CrossOrigin(origins = "https://portfolio-ef698.web.app/")
+
 public class CProyectos {
      @Autowired
     SProyectos sProyectos;
