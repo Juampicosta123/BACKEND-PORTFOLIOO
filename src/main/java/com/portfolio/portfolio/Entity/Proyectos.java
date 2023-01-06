16c16d2ed0babd9773ec0df2ad9fb152e811b542
Juampicosta123 <juampicosta81@gmail.com>
2023-01-06 18:18:05 -0300

    proyectos no anda

--- src/main/java/com/portfolio/portfolio/Entity/Proyectos.java
+++ src/main/java/com/portfolio/portfolio/Entity/Proyectos.java
@@ -8,14 +8,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Proyectos {
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private int id;
+   @NotNull
+   @Size(min = 1, max = 50, message = "no cumple con la longitud")
    private String nombre;
+   @NotNull
    private String descripcion;
+   @NotNull
    private String img;
    
     public Proyectos() {
